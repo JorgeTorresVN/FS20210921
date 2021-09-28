69916e09a7146fa6522248c479e64315e2dd0c48
JorgeTorresVN <Jorge.Torres@viewnext.com>
2021-09-28 18:03:59 +0200

    ejerciciosV0.5

--- Fundamentos/app/js/ejerciciosJS.js
+++ Fundamentos/app/js/ejerciciosJS.js
@@ -9,11 +9,51 @@ function ejercicio2(max,min){
     let a = prompt("Introduce un número:" ,0)
    
     for(i=0;i<9;i++){
-    if(a == num){
-        console.log("!HAS ACERTADO!, EL NUMERO CORRECTO ES: "+num)
+    a = prompt("Introduce un número:" ,0)  
+    if(a != num){
+        switch(a){
+            case (a<num):
+                prompt("El número es mayor")
+                case (a>num):
+                prompt("El número es menor")
+        }    
+        
     }else{
-    a = prompt("Introduce un número:" ,0)
+         alert("!HAS ACERTADO!, EL NUMERO CORRECTO ES: "+num)
+         return console.log("!HAS ACERTADO!, EL NUMERO CORRECTO ES: "+num)
+        }
+         
     }
 }   
-}
 
+function ejercicio3(b){
+    let a = 2;
+    let array = [];
+    for(i = a; i<b ;i++){
+        if(Primos(i)){
+        array.push(i)
+    }
+    return array;
+    
+}
+}
+function Primos(num){
+    if (num == 0|| num==1) return false;
+    for(i = 2; i<num;i++){
+        if(num%i==0) return false;   
+    }
+    return true;
+}
+function ejercicio6(){
+let cadena = prompt("Introduce una cadena:" ,0)
+let cad = cadena.split(" ").join("");
+let dac = "";
+for(i = cadena.length-1; i >= 0; i--){
+    dac += cadena.charAt(i);
+}
+if(cad.toUpperCase == dac.toUpperCase){
+    return console.log("Es Palindromo")
+}else{
+    return console.log("No es Palindromo")
+}
+}
