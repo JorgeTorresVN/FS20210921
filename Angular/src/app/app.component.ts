74e558580a8b31cbc75da2f966046ac604885bcc
JorgeTorresVN <Jorge.Torres@viewnext.com>
2021-10-14 15:50:59 +0200

    router

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationService } from './common-services';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+  constructor(navigation: NavigationService){}
 }
