686b15503203a956351257b339b06f4626d521bf
JorgeTorresVN <Jorge.Torres@viewnext.com>
2021-10-26 11:59:29 +0200

    REST Catalogo

--- Microservicios/catalogo/src/main/java/com/example/application/resource/FilmResource.java
+++ Microservicios/catalogo/src/main/java/com/example/application/resource/FilmResource.java
@@ -1,9 +1,40 @@
 package com.example.application.resource;
 
-public class FilmResource {
+import java.util.List;
 
-	public FilmResource() {
-		// TODO Auto-generated constructor stub
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.domains.contracts.services.ActorService;
+import com.example.domains.entities.dtos.ActorDTO;
+import com.example.domains.entities.dtos.FilmDTO;
+import com.example.exceptions.NotFoundException;
+
+@RestController
+@RequestMapping(path = "/api/peliculas")
+public class FilmResource {
+	@Autowired
+	ActorService srv;
+	
+	@GetMapping
+	public List<FilmDTO> getAll(@RequestParam(required = false) String sort) {
+		if(sort== null)
+			return srv.getByProjection(FilmDTO.class);
+		else
+			return (List<FilmDTO>) srv.getByProjection(Sort.by(sort), FilmDTO.class);
+	}
+	@GetMapping(params = "page")
+	public Page<FilmDTO> getAllPageable(Pageable item) {
+		return srv.getByProjection(item, FilmDTO.class);
 	}
 
+	
+	
 }
