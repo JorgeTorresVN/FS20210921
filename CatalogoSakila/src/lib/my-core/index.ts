c2aa77a21cb62ad6eeeffecaff5b6883fe8df2b9
JorgeTorresVN <Jorge.Torres@viewnext.com>
2021-11-01 16:47:54 +0100

    Catalogo

---
+++ CatalogoSakila/src/lib/my-core/index.ts
@@ -0,0 +1,4 @@
+export * from './my-core.module';
+export {ElipsisPipe,ToComaDecimalPipe} from './pipes/cadenas.pipe';
+export * from './services/logger.service';
+import {UppercaseValidation, } from './directives/validadores.directive';
